2595b1f40161b8934399e0019dacfbf00bd4e31f
sumirart <akun.pratamasumirat@gmail.com>
2019-03-08 22:49:06 +0700

    add post method to add new member

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
 const logger = require("./middleware/logger");
+
 const app = express();
 
 // Init middleware
-app.use(logger);
+// app.use(logger);
 
-// Gets all members
-app.get("/api/members", (req, res) => res.json(members));
-
-// get single member
-app.get("/api/member/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res
-      .status(400)
-      .json({ message: `Member with id ${req.params.id} not found` });
-  }
-});
+// use body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/api/members.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const uuid = require("uuid");
+const router = express.Router();
+const members = require("../../Members");
+
+// Gets all members
+router.get("/", (req, res) => res.json(members));
+
+// get single member
+router.get("/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res
+      .status(400)
+      .json({ message: `Member with id ${req.params.id} not found` });
+  }
+});
+
+// create member
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active"
+  };
+
+  if (!newMember.name || !newMember.email) {
+    return res
+      .status(400)
+      .json({ message: "Please insert name and email brooo!" });
+  }
+
+  members.push(newMember);
+  res.status(200).json(members);
+});
+
+module.exports = router;
